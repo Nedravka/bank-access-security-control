9193e9ec41bc3ea4184f7a49087f56ec2b05037a
Nedravka <zakalsergey@mail.ru>
2022-05-19 21:38:12 +0400

    add default param to .env variables and correct README.md

--- project/settings.py
+++ project/settings.py
@@ -9,7 +9,7 @@ load_dotenv()
 env = Env()
 env.read_env()
 
-DEFAULT_DATABASE_URL = f'postgres://{os.getenv("USER")}:' \
+DEFAULT_DATABASE_URL = f'{os.getenv("DB_ENGINE")}://{os.getenv("USER")}:' \
                       f''f'{os.getenv("PASSWORD")}@{os.getenv("HOST")}:' \
                       f''f'{os.getenv("PORT")}/{os.getenv("NAME")}'
 
@@ -19,14 +19,14 @@ DATABASES = {
 
 INSTALLED_APPS = ['datacenter']
 
-SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = os.getenv('SECRET_KEY', default='easy_key')
 
-DEBUG = bool(strtobool(os.getenv('DEBUG', default=False)))
+DEBUG = bool(strtobool(os.getenv('DEBUG', default='FALSE')))
 
 
 ROOT_URLCONF = 'project.urls'
 
-ALLOWED_HOSTS = env.list('ALLOWED_HOSTS')
+ALLOWED_HOSTS = env.list('ALLOWED_HOSTS', default='*')
 
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -41,9 +41,9 @@ TEMPLATES = [
 
 USE_L10N = True
 
-LANGUAGE_CODE = os.getenv('LANGUAGE_CODE')
+LANGUAGE_CODE = os.getenv('LANGUAGE_CODE', default='ru-ru')
 
-TIME_ZONE = os.getenv('TIME_ZONE')
+TIME_ZONE = os.getenv('TIME_ZONE', default='Europe/Moscow')
 
 USE_TZ = True
 
